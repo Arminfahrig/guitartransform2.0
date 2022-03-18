996bc37be0a2dbe51f93be780336e4ea44ab1869
armin <fzuvjh>
2022-03-18 22:01:53 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,345 @@
+let a , b , c , d , e, f, g;
+let  eingabe ;
+
+alert("Guten Tag, ich hoffe, diese Website kann dir ein wenig weiterhelfen!")
+
+function TonicaA() {
+ eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+}
+
+function TonicaB() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+}
+
+function TonicaD() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+}
+
+function TonicaE() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+}
+
+function TonicaF() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("f")
+        document.getElementById("tf2").value = "f"
+    }
+}
+
+function TonicaG() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("g")
+        document.getElementById("tf2").value = "g"
+    }
+}
+
+function SubdominanteA() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+}
+
+function SubdominanteB() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("f")
+        document.getElementById("tf2").value = "f"
+    }
+}
+
+function SubdominanteD() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+}
+
+function SubdominanteE() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+}
+
+function SubdominanteF() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+}
+
+
+
+function SubdominanteG() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+}
+
+function DominanteA() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+}
+
+function DominanteB() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("eb")
+        document.getElementById("tf2").value = "eb"
+    }
+}
+
+function DominanteD() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("g")
+        document.getElementById("tf2").value = "g"
+    }
+}
+
+function DominanteE() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+}
+
+function DominanteF() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+}
+
+function DominanteG() {
+    eingabe = document.getElementById("tf1").innerText = eingabe ;
+    if (eingabe === d) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+}
+/*
+window.onbeforeunload = function websiteVerlassen() {
+
+    return 'Danke, dass du vorbeigeschaut hast. Hab noch einen schönen Tag!';
+};
+function akkordeDzuB() {
+
+
+    if (eingabe === d) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+    else if (eingabe === a) {
+        console.log("fis")
+        document.getElementById("tf2").value = "fis"
+    }
+    else if(eingabe === g) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+}
+
+function akkordeDzuC() {
+
+    if (eingabe === d) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+    else if (eingabe === a) {
+        console.log("g")
+        document.getElementById("tf2").value =  "g"
+    }
+    else if(eingabe === g) {
+        console.log("f")
+        document.getElementById("tf2").value = "f"
+    }
+}
+
+function akkordeDzuE() {
+
+
+    if (eingabe === d) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+    else if (eingabe === a) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+    else if(eingabe === g) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+}
+
+
+function akkordeDzuF() {
+
+
+    if (eingabe === d) {
+        console.log("f")
+        document.getElementById("tf2").value = "f"
+    }
+    else if (eingabe === a) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+    else if(eingabe === g) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+}
+
+function akkordeDzuG() {
+
+
+    if (eingabe === d) {
+        console.log("g")
+        document.getElementById("tf2").value = "g"
+    }
+    else if (eingabe === a) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+    else if(eingabe === g) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+}
+
+function akkordeCzuA() {
+
+
+    if (eingabe === c) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+    else if (eingabe === g) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+    else if (eingabe === f) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+}
+
+function akkordeCzuB() {
+
+
+    if (eingabe === c) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+    else if (eingabe === g) {
+        console.log("fis")
+        document.getElementById("tf2").value = "fis"
+    }
+    else if (eingabe === f) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+}
+
+function akkordeCzuD() {
+
+
+    if (eingabe === c) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+    else if (eingabe === g) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+    else if (eingabe === f) {
+        console.log("g")
+        document.getElementById("tf2").value = "g"
+    }
+}
+
+
+function akkordeCzuE() {
+
+
+    if (eingabe === c) {
+        console.log("e")
+        document.getElementById("tf2").value = "e"
+    }
+    else if (eingabe === g) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+    else if (eingabe === f) {
+        console.log("a")
+        document.getElementById("tf2").value = "a"
+    }
+}
+
+
+function akkordeCzuF() {
+
+
+    if (eingabe === c) {
+        console.log("f")
+        document.getElementById("tf2").value = "f"
+    }
+    else if (eingabe === g) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+    else if (eingabe === f) {
+        console.log("b")
+        document.getElementById("tf2").value = "b"
+    }
+}
+
+
+function akkordeCzuG() {
+
+
+    if (eingabe === c) {
+        console.log("g")
+        document.getElementById("tf2").value = "g"
+    }
+    else if (eingabe === g) {
+        console.log("d")
+        document.getElementById("tf2").value = "d"
+    }
+    else if (eingabe === f) {
+        console.log("c")
+        document.getElementById("tf2").value = "c"
+    }
+}
+*/
